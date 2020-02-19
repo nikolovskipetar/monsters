0d521367411dfac43d464b922105132c9019833f
nikolovskipetar <petarnikolovski@hotmail.com>
2020-02-19 12:14:41 +0100

    adding new react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//Importing custom components
+import {CardList} from "./components/card-list/card-list.js";
+import {SearchBox} from "./components/serach-box/search-box";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); //calls the constructor of component class
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+  }
+
+  //life cycle method -> fetching users from api
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        //after fetching the response return to json format
+        .then(users => this.setState({monsters: users}))
+        //putting json data to the monsters array in constructor
+  }
+
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render() {
+    //easier way for ex. const monsters = this.state.monsters;
+    const { monsters, searchField } =  this.state;
+    const filterMonsters = monsters.filter(monster => (
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ));
+
+    return (
+        //JSX syntax (HTML inside JS)
+        <div className="App">
+          <h1>Monster Rolodex</h1>
+          <SearchBox placeholder='Search for monsters...' handleChange={this.handleChange}/>
+          <CardList monsters={filterMonsters}/>
+        </div>
+    );
+  }
 }
 
 export default App;
