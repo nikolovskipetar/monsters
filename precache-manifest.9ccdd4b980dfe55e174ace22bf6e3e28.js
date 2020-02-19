bd737dd551079b1e2dbd3954433930b95100067d
nikolovskipetar <petarnikolovski@hotmail.com>
2020-02-19 12:23:55 +0100

    Updates

---
+++ precache-manifest.9ccdd4b980dfe55e174ace22bf6e3e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a692ae5adf1472dd0fad610798113d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0f2fd3e5f619e972dd3",
+    "url": "/static/css/main.098d1432.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f2fd3e5f619e972dd3",
+    "url": "/static/js/main.d0e2ea5b.chunk.js"
+  },
+  {
+    "revision": "863018210bf1c6ae9c68",
+    "url": "/static/js/runtime-main.215bd54d.js"
+  }
+]);
